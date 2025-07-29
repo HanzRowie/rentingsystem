84b777b60b4643be23ed4fc914dc1ddb8574193f
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-07-29 19:43:00 +0545

    CustomUserregistered

--- myrentingsystem/accounts/admin.py
+++ myrentingsystem/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from accounts.models  import CustomUser, Profile
 
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('role', 'is_staff', 'is_active')
+
