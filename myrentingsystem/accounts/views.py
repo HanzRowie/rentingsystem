d0e4045ba19a9694546718aae0cbbe6a3849e8de
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-07-30 09:24:21 +0545

    registerviewcreated

--- myrentingsystem/accounts/views.py
+++ myrentingsystem/accounts/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
-from accounts.models import CustomUser
+from accounts.models import CustomUser, Profile
+from django.contrib.auth import authenticate,login,logout
+from accounts.serializers import RegisterSerializer, LoginSerializer
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = RegisterSerializer(data=data)
+            
+            if not serializer.is_valid():
+                return Response({
+                    'data': serializer.errors,
+                    'message': 'Something went wrong'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            serializer.save()
+            return Response({
+                'data': {},
+                'message': "Your account was successfully created"
+            }, status=status.HTTP_201_CREATED)
+
+        except Exception as e:
+            return Response({
+                'data': str(e),
+                'message': "An error occurred"
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
