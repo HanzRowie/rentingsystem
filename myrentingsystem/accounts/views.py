0e481e781bd93252d6e545b50774f057f8de69f0
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-07-30 09:37:43 +0545

    loginviewcreated

--- myrentingsystem/accounts/views.py
+++ myrentingsystem/accounts/views.py
@@ -34,3 +34,22 @@ class RegisterView(APIView):
                 'message': "An error occurred"
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+class LoginView(APIView):
+    def post(self,request):
+        try:
+            serializer  =  LoginSerializer(data=request.data)
+            if not serializer.is_valid():
+                
+                return Response({
+                    'data':serializer.errors,
+                    'message':'Something went wrong'
+                },status=status.HTTP_400_BAD_REQUEST)
+            
+            token_response = serializer.get_jwt_token(serializer.validate_data)
+            return Response(token_response,status=status.HTTP_200_OK)
+        
+        except Exception as e:
+            return Response({
+                'data':{},
+                'message':"An error occured"
+            }, status=status.HTTP_400_BAD_REQUEST)
