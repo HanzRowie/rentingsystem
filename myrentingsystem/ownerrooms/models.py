f6d35baa29a437601dc6c80e3eba29f1034f35a5
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-07-31 10:11:45 +0545

    Room model created in ownerroom

--- myrentingsystem/ownerrooms/models.py
+++ myrentingsystem/ownerrooms/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Room(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'role': 'owner'})
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
