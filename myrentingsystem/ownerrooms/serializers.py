7404841259d8f6f57d1cc000ad8e2c3d95e44cb6
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-01 18:20:53 +0545

    get method created for room view

--- myrentingsystem/ownerrooms/serializers.py
+++ myrentingsystem/ownerrooms/serializers.py
@@ -1,2 +1,18 @@
 from rest_framework import serializers
 from ownerrooms.models import Room
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ['id', 'owner', 'title', 'description', 'location', 'price', 'available', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+    def validate(self, data):
+        if data['price'] <= 0:
+            raise serializers.ValidationError("Price must be a positive number.")
+        return data
+
+    def create(self, validated_data):
+        return Room.objects.create(**validated_data)
+
--- myrentingsystem/ownerrooms/views.py
+++ myrentingsystem/ownerrooms/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Room
+from ownerrooms.serializers import RoomSerializer
+from rest_framework import ApiView, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
+
+class RoomView(ApiView):
+    permission_class = [IsAuthenticated]
+    def get(self,request,pk = None):
+        user = request.user
+        if pk is not None:
+            room = get_object_or_404(Room,pk=pk, user=user)
+            serializer = RoomSerializer(room)
+        else:
+            room = Room.objects.filter(user=user)
+            serializer = RoomSerializer(room, many = True)
+
+        return Response(serializer.data)
+  
+        
