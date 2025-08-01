983288aabf77da0f123c2941a00d2258850877d6
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-01 18:27:13 +0545

    post method created for room view

--- myrentingsystem/ownerrooms/views.py
+++ myrentingsystem/ownerrooms/views.py
@@ -19,5 +19,15 @@ class RoomView(ApiView):
             serializer = RoomSerializer(room, many = True)
 
         return Response(serializer.data)
+      
+    def post(self,request):
+        serializer = RoomSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({'msg':'Room successfully posted'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+
+
   
         
