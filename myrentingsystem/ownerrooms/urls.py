16be8684e88d50bc4e084dbae9dea7218d020e51
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-03 19:46:05 +0545

    Added RoomView URLs in ownerrooms app

--- myrentingsystem/ownerrooms/urls.py
+++ myrentingsystem/ownerrooms/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-
+    path('roomview/', views.RoomView.as_view(), name='room_view'),
+    path('roomview/<int:pk>/', views.RoomView.as_view(), name='room_detail'),
 ]
