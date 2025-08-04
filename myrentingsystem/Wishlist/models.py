265ee51bd2a83c5a315441c5b99ecac2abfb167d
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-04 21:47:33 +0545

    Wishlist models created in Wishlist app

--- myrentingsystem/Wishlist/models.py
+++ myrentingsystem/Wishlist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from ownerrooms.models import Room
+from django.conf import settings
 
-# Create your models here.
+class Wishlist(models.Model):
+    seeker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'role': 'seeker'}, related_name='wishlist_items')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('seeker', 'room')  
+
+    def __str__(self):
+        return f"{self.seeker.username} wishlist -> {self.room.title}"
--- myrentingsystem/myrentingsystem/urls.py
+++ myrentingsystem/myrentingsystem/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')), 
     path('ownerrooms/', include('ownerrooms.urls')), 
     path('seeker/', include('seeker.urls')),
+    path('wishlist/', include('Wishlist.urls')),
 ]
