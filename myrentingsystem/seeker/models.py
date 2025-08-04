2a6010cc6e8451e67b110b44bf9df82158956d0e
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-04 09:00:02 +0545

    RoomRequest models created in seeker app

--- myrentingsystem/seeker/models.py
+++ myrentingsystem/seeker/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class RoomRequest(models.Model):
+    seeker = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, limit_choices_to={'role': 'seeker'})
+    room = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    message = models.TextField(blank=True)
+    requested_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.seeker.username} -> {self.room.title}"
