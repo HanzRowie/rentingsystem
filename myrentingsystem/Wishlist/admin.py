1e53df57d4e0d1f436c55e5c3caeec68c775ecdc
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-04 21:50:34 +0545

    Register Wishlist model in Django admin interface for management and visibility.

--- myrentingsystem/Wishlist/admin.py
+++ myrentingsystem/Wishlist/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Wishlist.models import Wishlist
 
 # Register your models here.
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('seeker', 'room', 'added_at')
+    search_fields = ('seeker__username', 'room__title')
+    list_filter = ('added_at',)
+    ordering = ('-added_at',)
