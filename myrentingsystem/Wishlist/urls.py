edfbbddfedf67e3424def9246a865b07d5f2d273
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-05 09:04:46 +0545

    urls created for WishlistView

--- myrentingsystem/Wishlist/urls.py
+++ myrentingsystem/Wishlist/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from Wishlist.views import WishlistView
 
 
 urlpatterns = [
+    path('wishlist/', WishlistView.as_view(), name='wishlist-list'),
+    path('wishlist/<int:room_id>/', WishlistView.as_view(), name='wishlist-add'),
 ]
