ddc8a40054427c880e4fa3d4ca5bc3d9967948b3
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-06 20:58:42 +0545

    UserChangePasswordView created in accounts views file

--- myrentingsystem/accounts/views.py
+++ myrentingsystem/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from accounts.models import CustomUser, Profile
 from django.contrib.auth import authenticate,login,logout
-from accounts.serializers import RegisterSerializer, LoginSerializer, ProfileSerializers
+from accounts.serializers import RegisterSerializer, LoginSerializer, ProfileSerializers, UserChangePasswordSerializer
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
@@ -112,4 +112,11 @@ class ProfileView(APIView):
             "msg":"Profile successfully deleted"
         },status=status.HTTP_204_NO_CONTENT)
     
-    
+
+class  UserChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request,format = None):
+        serializer  = UserChangePasswordSerializer(data = request.data, context = {'user':request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({'msg':'password changed successfully'}, status=status.HTTP_200_OK)
+        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
