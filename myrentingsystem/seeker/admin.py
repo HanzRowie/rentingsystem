559e552579932ea5473a9b31d5baf7e67f188242
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-08 22:08:33 +0545

    admin file updated in seeker app

--- myrentingsystem/seeker/admin.py
+++ myrentingsystem/seeker/admin.py
@@ -5,8 +5,8 @@ from seeker.models import RoomRequest
 
 @admin.register(RoomRequest)
 class RoomRequestAdmin(admin.ModelAdmin):
-    list_display = ('seeker', 'room', 'requested_at', 'is_approved')
-    list_filter = ('is_approved', 'requested_at')
+    list_display = ('seeker', 'room', 'requested_at', 'status')
+    list_filter = ('status', 'requested_at')
     search_fields = ('seeker__username', 'room__title')
     raw_id_fields = ('seeker', 'room')
     ordering = ('-requested_at',)
