186fe4d0ad34f6e695ea14a0b473548a2ee8d520
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-08 22:58:50 +0545

    ownerrooms urls updated

--- myrentingsystem/ownerrooms/urls.py
+++ myrentingsystem/ownerrooms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from ownerrooms.views import RoomView
+from ownerrooms.views import RoomView, OwnerRoomRequestView
 
 urlpatterns = [
     path('roomview/', RoomView.as_view(), name='room_view'),
     path('roomview/<int:pk>/',RoomView.as_view(), name='room_detail'),
+    path('ownerroomrequests/', OwnerRoomRequestView.as_view(), name='owner_room_requests'),
 ]
