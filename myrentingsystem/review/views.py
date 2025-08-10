c03e2d52d0a63a5d83cfdc7bc81b47733ba6eae2
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-10 08:05:13 +0545

    Put method added in ReviewView in review view file

--- myrentingsystem/review/views.py
+++ myrentingsystem/review/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from  rest_framework.permissions import IsAuthenticated, AllowAny
@@ -15,7 +15,7 @@ class ReviewView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, room_id):
-        # ✅ Ensure the seeker has a RoomRequest for this room
+     
         has_requested = RoomRequest.objects.filter(
             seeker=request.user,
             room_id=room_id,
@@ -28,15 +28,25 @@ class ReviewView(APIView):
                 status=status.HTTP_403_FORBIDDEN
             )
 
-        # ✅ Create review
+       
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(seeker=request.user, room_id=room_id)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
     def get(self, request):
         reviews = Review.objects.all()
         serializer = ReviewSerializer(reviews, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def put(self,request, room_id):
+        review = get_object_or_404(Review,room_id = room_id, seeker=request.user)
+        serializer = ReviewSerializer(review,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg':"Review Updated Successfully"},status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
