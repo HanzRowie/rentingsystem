fa5d3ab6941bd23962ef5a5e758baa2c2a334955
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-10 07:30:56 +0545

    serializer file in reivew app updated

--- myrentingsystem/review/serializers.py
+++ myrentingsystem/review/serializers.py
@@ -7,9 +7,9 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['id', 'room', 'seeker', 'rating', 'comment', 'create_at']
-        read_only_fields = ['id', 'create_at']
+        read_only_fields = ['id', 'create_at', 'seeker', 'room']
 
     def validate_rating(self, value):
-        if value <1 or value  >5:
+        if value < 1 or value > 5:
             raise serializers.ValidationError("Rating must be between 1 and 5.")
         return value
