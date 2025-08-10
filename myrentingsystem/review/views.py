7b9fe284a127841c7aacb8f8832704c70185c17e
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-10 07:15:01 +0545

    ReviewView created in review view file

--- myrentingsystem/review/views.py
+++ myrentingsystem/review/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from  rest_framework.permissions import IsAuthenticated, AllowAny
+from review.models import Review
+from review.serializers import ReviewSerializer
+from ownerrooms.models import Room
+from seeker.models import Seeker
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class ReviewView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, room_id):
+        serializer = ReviewSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
