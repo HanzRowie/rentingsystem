09336d255e0ffb306d27af16c545adbc8063aba0
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-10 09:01:41 +0545

    Add URL route for room review summary view

--- myrentingsystem/review/urls.py
+++ myrentingsystem/review/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from review.views import ReviewView
+from review.views import ReviewView, RoomReviewViewSummaryView
 
 urlpatterns = [
     path('reviews/', ReviewView.as_view(), name='review_list'),
     path('reviews/<int:room_id>/', ReviewView.as_view(), name='review_create'),
+    path('reviewssummary/',RoomReviewViewSummaryView.as_view(), name='review_summary'),
 ]
