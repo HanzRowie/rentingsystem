729d4cb1f1c84798f4341c11149654de91d95a19
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-19 22:42:40 +0545

    Add notification URL patterns for listing and marking notifications as read

--- myrentingsystem/notification/urls.py
+++ myrentingsystem/notification/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from notification.views import NotificationListView, NotificationMarkReadView
 
 urlpatterns = [
-    
+    path('notifications/', NotificationListView.as_view(), name='notification-list'),
+    path('notifications/<int:pk>/mark-read/', NotificationMarkReadView.as_view(), name='notification-mark-read'),
 ]
