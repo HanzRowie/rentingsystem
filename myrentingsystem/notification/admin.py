6bf7fe2fd45ca0500e9b6354e54dc789d1cf7500
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-19 09:26:38 +0545

    Add NotificationAdmin to manage Notification model in Django admin

---
+++ myrentingsystem/notification/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Notification
+
+# Register your models here.
+
+admin.site.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'is_read', 'created_at')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('title', 'message')
+    ordering = ('-created_at',)
