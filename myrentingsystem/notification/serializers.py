0f16d7d25a95396c1dab6e6ee891c0da5a043102
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-08-29 13:58:46 +0545

    Fix read_only_fields assignment in NotificationSerializer for clarity

--- myrentingsystem/notification/serializers.py
+++ myrentingsystem/notification/serializers.py
@@ -5,6 +5,6 @@ class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
         fields = ['id', 'user', 'title', 'message', 'is_read', 'created_at']
-        read_only_fields = fields
+        read_only_fields = ['id', 'user', 'title', 'message', 'created_at']
 
         
