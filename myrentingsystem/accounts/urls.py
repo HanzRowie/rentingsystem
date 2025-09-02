a053e49c37dc1080ec29437f97c3048156d874ea
Hanz Rowie <hanzrowiegurung@gmail.com>
2025-09-02 20:57:40 +0545

    Refactor URL patterns for improved readability

--- myrentingsystem/accounts/urls.py
+++ myrentingsystem/accounts/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
+  
    path('register/',views.RegisterView.as_view(),name='register'),
    path('login/',views.LoginView.as_view(),name='login'),  
    path('profile/',views.ProfileView.as_view(),name='profile'),
+   path('refresh/', views.CustomTokenRefreshView.as_view(), name='token_refresh'),
    path('logout/',views.LogoutView.as_view(),name='logout'),
    path('changepassword/', views.UserChangePasswordView.as_view(), name='changepassword'),
    path('UserPasswordResetView/<uid>/<token>/', views.UserPasswordReset.as_view(), name='UserPasswordResetView'),
